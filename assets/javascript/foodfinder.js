3008e5a58418d4191feeb8d19d0150b377c35b35
amber wade <amberwade@ambers-MacBook-Pro.local>
2018-06-06 09:34:26 -0700

    ccsfixattempt

--- assets/javascript/foodfinder.js
+++ assets/javascript/foodfinder.js
@@ -125,7 +125,7 @@ function initMap() {
   var location ={lat: -25.363, lng: 131.044};
   var map = new google.maps.Map(document.getElementById("map"),
 {
-    zoom: 4,
+    zoom: 11,
     center: location
 });
 
